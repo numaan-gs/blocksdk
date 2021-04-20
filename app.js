97c5a83dd6b358cf98df517f44b1f527e8b3dddc
numaan-gs <Numaan.Mohd@mkcorp.com>
2021-04-19 17:12:19 -0700

    3rd commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
