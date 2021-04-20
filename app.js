97821390109a57fbb59ddfd120db40a1a27119ae
numaan-gs <Numaan.Mohd@mkcorp.com>
2021-04-20 10:47:38 -0700

    structure commit

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.sendFile('./blocks/plain-text.html', { root: __dirname });
+    res.sendFile('./views/plain-text.html',{ root: __dirname });
+});
+
+app.get('/blocksdk', (req, res) => {
+    res.sendFile('./scripts/blocksdk.js',{ root: __dirname });
+});
+
+app.get('/plain-text', (req, res) => {
+    res.sendFile('./views/plain-text.html',{ root: __dirname });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
