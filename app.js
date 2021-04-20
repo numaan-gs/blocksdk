a20874df341d84015947b300aba37b83facaa1e6
numaan-gs <Numaan.Mohd@mkcorp.com>
2021-04-19 17:33:13 -0700

    plain text added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('./blocks/plain-text.html', { root: __dirname });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
