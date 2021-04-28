f664e7f2663022530c07f2a927a91e39813391ac
numaan-gs <Numaan.Mohd@mkcorp.com>
2021-04-27 22:46:21 -0700

    final change

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const sldsAssets = '/node_modules/@salesforce-ux/design-system/assets';
 const blockSdk = '/node_modules/blocksdk'; 
+const services = '/services'; 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/slds', express.static(__dirname + sldsAssets));
 app.use('/blocksdk', express.static(__dirname + blockSdk));
+app.use('/services', express.static(__dirname + services));
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/plain-text.html',{ root: __dirname });
+    res.sendFile('./views/plainTextCustomBlock.html',{ root: __dirname });
 });
 
-app.get('/blocksdk', (req, res) => {
-    res.sendFile('./scripts/blocksdk.js',{ root: __dirname });
-});
-
-app.get('/plain-text', (req, res) => {
-    res.sendFile('./views/plain-text.html',{ root: __dirname });
+app.get('/plaintextcustomblock', (req, res) => {
+    res.sendFile('./views/plainTextCustomBlock.html',{ root: __dirname });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
